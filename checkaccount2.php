71171ff483b05213fac82d0f8b73e375c1a8e6e0
Non One <non_111111@hotmail.com>
2019-11-02 16:22:44 +1100

    first commit

---
+++ checkaccount2.php
@@ -0,0 +1,58 @@
+<html>
+<body>
+
+<?php
+
+$username = $_POST["username"];
+$password = $_POST["password"];
+
+$dbserver = "localhost";
+$dbusername = "test";
+$dbpassword = "p@ssw0rd";
+$db = "test";
+
+$conn = new mysqli($dbserver, $dbusername, $dbpassword, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+
+// print account details
+
+$sql = "SELECT * FROM users_data WHERE username=? AND password=?";
+
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("ss", $username, $password);
+$stmt->execute();
+
+$result = $stmt->get_result();
+
+if ($result->num_rows > 0) {
+
+	// print heading
+	echo "<H1>Your Account</H1>";
+	echo "<p>Welcome, your account details are:</p>";
+
+	while($row = $result->fetch_assoc()) {
+        echo " <p>Name: " . $row["lastname"]. ", " . $row["firstname"]. " <BR>Address: " . $row["address"]. "<br>Phone number: " . $row["phone"] . "</p>";
+    }
+}
+else
+{
+echo "<p><b>Login failed</b></p>";
+}
+
+
+$conn->close();
+
+
+echo "<p><BR><font color=\"red\">" . $sql . "</font></p>";
+
+
+//https://websitebeaver.com/prepared-statements-in-php-mysqli-to-prevent-sql-injection
+
+?>
+
+</body>
+</html>
---
+++ loginform2.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+<h1>Log in to your account</h1>
+
+<form action="checkaccount2.php" method="post">
+Log in to view your account details <br>
+Username: <input type="text" name="username" size="100"><br>
+Password: <input type="text" name="password" size="200"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
+
+
